c96183668e6fccf6af64a941811139eca17682db
homalurc <homalurc@asu.edu>
2017-10-14 08:04:25 -0700

    Created pickle file of movie represented in terms of all features

--- Code/task4/recommend.py
+++ Code/task4/recommend.py
@@ -1,60 +1,77 @@
 import pandas as pd
+import pickle
+
+def save_obj(obj, filename):
+	with open(filename, 'wb') as output:
+		pickle.dump(obj, output, pickle.HIGHEST_PROTOCOL)
+
 
 
 df_movies = pd.read_csv('../../Phase2_data/mlmovies.csv')
 df_tags = pd.read_csv('../../Phase2_data/genome-tags.csv')
 df_users = pd.read_csv('../../Phase2_data/mlusers.csv')
-df_actors = pd.read_csv('../../Phase2_data/movie-actor.csv')
+df_actors = pd.read_csv('../../Phase2_data/imdb-actor-info.csv')
 
 movies = df_movies.movieid.unique().tolist()
 tags = df_tags.tagId.unique().tolist()
 users = df_users.userid.unique().tolist()
-actors = df_actors.actorid.unique().tolist()
+actors = df_actors.id.unique().tolist()
 
 movie_genre = pd.DataFrame(df_movies.genres.str.split('|').tolist(), index = df_movies.movieid).stack()
 movie_genre = movie_genre.reset_index()[[0,'movieid']]
 movie_genre.columns = ['genres','movieid']
 genres = movie_genre.genres.unique().tolist()
 
-def check_tags(tags, mid):
-	df_mltags = pd.read_csv('../../Phase2_data/mltags.csv')
-	tag_values = []
-	for tag in tags:
-		if((df_mltags[['movieid','tagid']].values == [mid, tag]).all(axis=1).any()):
-			tag_values.append(1)
-		else:
-			tag_values.append(0)
-	return tag_values
+user_dict = dict()
+df_mlratings = pd.read_csv('../../Phase2_data/mlratings.csv')
+df_mltags = pd.read_csv('../../Phase2_data/mltags.csv')
+for row in df_mlratings.iterrows():
+	
+	if row[1]['movieid'] in user_dict:
+		user_dict[row[1]['movieid']].append(row[1]['userid'])
+	else:
+		user_dict[row[1]['movieid']] = [row[1]['userid']]
+print user_dict
 
-def check_users(users, mid):
+def check_tag(tags, mid):
 	df_mltags = pd.read_csv('../../Phase2_data/mltags.csv')
-	tag_values = []
+	values = []
 	for tag in tags:
 		if((df_mltags[['movieid','tagid']].values == [mid, tag]).all(axis=1).any()):
-			tag_values.append(1)
+			values.append(1)
 		else:
-			tag_values.append(0)
-	return tag_values
+			values.append(0)
+	return values
 
-def check_tags(tags, mid):
+def check_user(users, mid):
+	df_mlratings = pd.read_csv('../../Phase2_data/mlratings.csv')
 	df_mltags = pd.read_csv('../../Phase2_data/mltags.csv')
-	tag_values = []
-	for tag in tags:
-		if((df_mltags[['movieid','tagid']].values == [mid, tag]).all(axis=1).any()):
-			tag_values.append(1)
+	values = []
+	for user in users:
+		if(user in user_dict[mid]):
+			values.append(1)
 		else:
-			tag_values.append(0)
-	return tag_values
+			values.append(0)
+	return values
 
-def check_tags(tags, mid):
-	df_mltags = pd.read_csv('../../Phase2_data/mltags.csv')
-	tag_values = []
-	for tag in tags:
-		if((df_mltags[['movieid','tagid']].values == [mid, tag]).all(axis=1).any()):
-			tag_values.append(1)
+def check_genre(genres, mid):
+	values = []
+	for genre in genres:
+		if((movie_genre[['movieid','genres']].values == [mid, genre]).all(axis=1).any()):
+			values.append(1)
 		else:
-			tag_values.append(0)
-	return tag_values
+			values.append(0)
+	return values
+
+def check_actor(actors, mid):
+	df_mactors = pd.read_csv('../../Phase2_data/movie-actor.csv')
+	values = []
+	for actor in actors:
+		if((df_mactors[['movieid','actorid']].values == [mid, actor]).all(axis=1).any()):
+			values.append(1)
+		else:
+			values.append(0)
+	return values
 
 
 
@@ -65,8 +82,10 @@ print len(cols)
 df_movie_final = pd.DataFrame(columns=cols)
 
 for movie_id in movies:
-	tag_values = check_tags(tags, movie_id)
-	#genre_values = check_genre(genres, movie_id)
-	#user_values = check_user(users, movie_id)
-	#actor_values = check_actor(actors, movie_id)
-	#df_movie_final.loc[id] = []
+	tag_values = check_tag(tags, movie_id)
+	genre_values = check_genre(genres, movie_id)
+	user_values = check_user(users, movie_id)
+	actor_values = check_actor(actors, movie_id)
+	df_movie_final.loc[id] = tag_values + user_values + genre_values + actor_values
+print df_movie_final
+save_obj(df_movie_final, 'movie_final.pkl')

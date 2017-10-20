3177a63ca31ccca221750aa94d966758c6800a26
JASMIN GEORGE <jasmingeorge@JASMINs-MacBook-Pro.local>
2017-10-20 15:59:30 -0700

    Task1b spce of actors PCA

--- Code/task1b/util.py
+++ Code/task1b/util.py
@@ -1,30 +1,34 @@
 from datetime import datetime
 import pandas as pd
+from numpy import log
+import os
 
-db_folder = '../phase2_data/'
-mltags_file = 'mltags.csv'
+db_folder_path = os.path.join(os.path.dirname(__file__), "..", "..", "Phase2_data")
+output_folder = os.path.join(os.path.dirname(__file__), "..", "..", "Output")
+
 mlmovies_file = 'mlmovies.csv'
-genome_tags_file = 'genome-tags.csv'
+movie_actor_file = 'movie-actor.csv'
+imdb_actor_info_file = 'imdb-actor-info.csv'
 
 
 ############### HELPER FUNCTION TO READ FILES #################################
 
-# import mltags
-def read_mltags():
-	mltags =  pd.read_csv(db_folder + mltags_file)	
-	current_time = datetime.now()
-	for i,row in mltags.iterrows():
-		mltags.set_value(i,'timestamp', (datetime.strptime(row['timestamp'],'%Y-%m-%d %H:%M:%S') - datetime.fromtimestamp(0)).total_seconds()/
-			(current_time - datetime.fromtimestamp(0)).total_seconds())
-	return mltags
-
 # import mlmovies
 def read_mlmovies():
-	return pd.read_csv(db_folder + mlmovies_file)
+	return pd.read_csv(os.path.abspath(os.path.join(db_folder_path, mlmovies_file)))
 
-# import genome-tags
-def read_genome_tags():
-	return pd.read_csv(db_folder + genome_tags_file)
+# import imdb-actor-info
+def read_imdb_actor_info():
+	return pd.read_csv(os.path.abspath(os.path.join(db_folder_path, imdb_actor_info_file)))
+
+# import movie-actor
+def read_movie_actor():
+	movie_actor = pd.read_csv(os.path.abspath(os.path.join(db_folder_path, movie_actor_file)))
+	max_rank = movie_actor[['actor_movie_rank']].apply(max)[0]
+	min_rank = movie_actor[['actor_movie_rank']].apply(min)[0]
+	for i,row in movie_actor.iterrows():
+		movie_actor.iloc[i,2] = 1/float(row['actor_movie_rank'])
+	return movie_actor
 
 ################## HELPER FUNCTION TO PROCESS AND RETRIEVE ######################
 
@@ -34,31 +38,59 @@ def get_movie_genre(mlmovies):
 	movie_genre.columns = ['genre','movieid']
 	return movie_genre
 
-def filter_by_genre(mltags, mlmovies, genre):
+def filter_by_genre(movie_actor, mlmovies, genre):
 	movie_genre = get_movie_genre(mlmovies)
 	movieids_of_genre = movie_genre.where(movie_genre['genre']==genre).dropna().loc[:,'movieid'].unique()
-	mltags_of_genre = mltags.where(mltags['movieid'].isin(movieids_of_genre)).dropna()
-	genre_movie_count = movieids_of_genre.shape[0]
+	actors_of_genre = movie_actor.where(movie_actor['movieid'].isin(movieids_of_genre)).dropna()
+	genre_movie_count = int(movieids_of_genre.shape[0])
 
-	return (genre_movie_count, mltags_of_genre)
+	return (genre_movie_count, actors_of_genre)
 
 def get_tf_idf_matrix(genre):
-	mltags = get_mltags()
-	mlmovies = get_mlmovies()
+	movie_actor = read_movie_actor()
+	mlmovies = read_mlmovies()
 	movie_genre = get_movie_genre(mlmovies)
-	genre_movie_count, mltags_of_genre = filter_by_genre(mltags, mlmovies, genre)
+	genre_movie_count, actors_of_genre = filter_by_genre(movie_actor, mlmovies, genre)
 
-	# Needed for TF denominators
-	tags_per_movie = mltags_of_genre.groupby('movieid', as_index=False)['tagid'].agg({'m_count' : 'count'})
+	# Needed for TF denominators. Calcuate total number of actors per movie
+	actors_per_movie = actors_of_genre.groupby('movieid', as_index=False)['actorid'].agg({'m_count' : pd.Series.count})
 
-	# Needed for IDF denonminators
-	movies_per_tag = mltags_of_genre.groupby('tagid', as_index=False)['movieid'].agg({'t_count' : pd.Series.nunique})
+	# Needed for IDF denonminators. Calculate unique movieids for each actor
+	movies_per_actor = actors_of_genre.groupby('actorid', as_index=False)['movieid'].agg({'a_count' : pd.Series.nunique})
 
-	tagid_movieid_grouped = mltags_of_genre.groupby(['tagid', 'movieid'], as_index=False).agg({'timestamp': 'sum'})
+	# Grouped so as to create unique actorid, movieid pairs and calculate term frequency from actor_movie_rank
+	actor_movieid_grouped = actors_of_genre.groupby(['actorid', 'movieid'], as_index=False)['actor_movie_rank'].agg({'tf': 'sum'})
 
-	merged1 = pd.merge(tagid_movieid_grouped, tags_per_movie, on=['movieid','movieid'], how='inner')
-	merged2 = pd.merge(merged1, movies_per_tag, on=['tagid', 'tagid'], how = 'inner')
-	print merged2
-	#tbl2 = tbl.pivot(index='movieid', columns='tagid', values='timestamp').fillna(0)
-	#print tbl2
+	# Merge actor_counts. Add new column including calculated actors_per_movie.
+	M1 = pd.merge(actor_movieid_grouped, actors_per_movie, on=['movieid','movieid'], how='inner')
+
+	# Merge movie_counts. Add new column including calculated movies_per_actor.
+	M2 = pd.merge(M1, movies_per_actor, on=['actorid', 'actorid'], how = 'inner')
+
+	# Perform TF-IDF from the data.
+	M2['tfidf'] = M2['tf']*log(genre_movie_count/M2['m_count'])/M2['a_count']
+	#print M2
+
+	# Pivot the matrix to get in required form 
+	R = M2.pivot(index='movieid', columns='actorid', values='tfidf').fillna(0)
+	#print R
+	return R
+
+def print_output(genre, concepts):
+	print('For genre: ' + genre + ', output is :')
+	for idx, concept in enumerate(concepts):
+		print ('\nThe ' + str(idx+1) +'th concept is: ')
+		print('%40s\t%15s\t' %('Actor', 'Weight'))
+		for row in concept:
+			print ('%40s\t%15s\t' %(row[0], row[1]))
+
+def write_output_file(genre, concepts, filename):
+	f = open(os.path.abspath(os.path.join(output_folder, filename)),'w')
+	f.write('For genre: ' + genre + ', output is :' + '\n')
+	for idx, concept in enumerate(concepts):
+		f.write('\nThe ' + str(idx+1) +'th concept is: ')
+		f.write('\n%40s\t%15s\t' %('Actor', 'Weight'))
+		for row in concept:
+			f.write('\n%40s\t%15s\t' %(row[0], row[1]))
+	f.close()
 
